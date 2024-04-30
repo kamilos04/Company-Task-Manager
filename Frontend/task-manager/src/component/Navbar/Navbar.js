c837c5fb01cf21214839092da12b927aa82631e0
Kamil Jach <kamiljach2004@gmail.com>
2024-04-30 22:24:33 +0200

    Logout button added

--- Frontend/task-manager/src/component/Navbar/Navbar.js
+++ Frontend/task-manager/src/component/Navbar/Navbar.js
@@ -2,10 +2,14 @@ import { Button, Stack } from '@mui/material'
 import React from 'react'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { useLocation, useNavigate } from 'react-router-dom';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { useDispatch } from 'react-redux';
+import { logoutUser } from '../State/Authentication/Action';
 
 const Navbar = () => {
   const navigate = useNavigate()
   const location = useLocation()
+  const dispatch = useDispatch()
   return (
     <div className='w-[100%] shadow-lg  bg-blue-500 flex flex-row h-[4rem] justify-between items-center'>
       <div className='flex flex-row items-center'>
@@ -14,8 +18,9 @@ const Navbar = () => {
         <Button onClick={() => {navigate("/tasks")}} className={`ml-2  ${location.pathname==="/tasks" ? "text-green-300" : "text-white"} text-lg normal-case`} variant="text">Tasks</Button>
       </div>
 
-      <div>
+      <div className='flex flex-row'>
         <AccountCircleIcon className='text-4xl mr-6 block cursor-pointer' />
+        <LogoutIcon onClick={() => {dispatch(logoutUser(navigate))}} className='text-4xl mr-4 block cursor-pointer'/>
       </div>
     </div>
   )
