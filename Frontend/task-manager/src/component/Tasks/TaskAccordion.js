56527f8a7d1d4f1065ecad5f1d55cfaa7cf0afdc
Kamil Jach <kamiljach2004@gmail.com>
2024-05-20 19:23:56 +0200

    Update task button added, permission checking added

--- Frontend/task-manager/src/component/Tasks/TaskAccordion.js
+++ Frontend/task-manager/src/component/Tasks/TaskAccordion.js
@@ -1,14 +1,39 @@
-import { Accordion, AccordionDetails, AccordionSummary, ToggleButton, ToggleButtonGroup } from '@mui/material'
+import { Accordion, AccordionDetails, AccordionSummary, Button, ToggleButton, ToggleButtonGroup } from '@mui/material'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import React from 'react'
 import PriorityChip from './PriorityChip';
 import StatusChip from './StatusChip';
 import DescChip from './DescChip';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { updateTaskStatus } from '../State/Tasks/Action';
+import { useNavigate } from 'react-router-dom';
 
 const TaskAccordion = (props) => {
+    const navigate = useNavigate()
     const dispatch = useDispatch()
+    const auth = useSelector(store => store.auth)
+
+    const handleClickUpdateTask = () => {
+        navigate(`/update-task/${props.task.id}`)
+    }
+
+    const ifUserHasPermissionToEditTask = () => {
+        let permission = false
+        let usersTeamsAdminNames = auth.profile.teamsAdmin.map((team) => team.name)
+        console.log(usersTeamsAdminNames)
+        props.task.teams.forEach((team) => {
+            if(usersTeamsAdminNames.includes(team.name)){
+                permission = true
+            }
+        })
+        props.task.admins.forEach((admin) => {
+            if(admin.id === auth.profile.id){
+                permission = true
+            }
+        })
+        console.log(permission)
+        return permission
+    }
 
     const handleStatusChange = (task) => (event, newValue) => {
         const taskData = {
@@ -79,6 +104,7 @@ const TaskAccordion = (props) => {
                         </div>
                     </div>
                     <div className='flex justify-end w-full'>
+                    {ifUserHasPermissionToEditTask() && <Button variant="contained" className='mr-3' onClick={handleClickUpdateTask}>Update</Button>}
                         <ToggleButtonGroup
                             value={props.task.status}
                             exclusive
