d10b5e0ff5194f5f0a998cc45de928f74d28f46d
Kamil Jach <kamiljach2004@gmail.com>
2024-05-28 10:55:53 +0200

    Mobile NavBar appearance changed

--- Frontend/task-manager/src/component/Navbar/Navbar.js
+++ Frontend/task-manager/src/component/Navbar/Navbar.js
@@ -23,6 +23,7 @@ const Navbar = () => {
   }
   return (
     <div>
+      {/* Desktop */}
       <div className='hidden lg:flex w-[100%] shadow-lg flex-row h-16 justify-between items-center' style={{ borderBottom: '1px solid rgb(84, 87, 92)' }}>
         <div className='flex flex-row items-center'>
           <div className='ml-8 text-3xl text-white mr-8 fontlogo'>COMPANY TASK MANAGER</div>
@@ -39,6 +40,7 @@ const Navbar = () => {
         </div>
       </div>
 
+      {/* Mobile */}
       <div className='flex lg:hidden w-[100%] fixed flex-col justify-between items-center z-10 bg-[rgb(24,28,44)]' style={{ borderBottom: '1px solid rgb(84, 87, 92)' }}>
         <div className='flex justify-between  w-full  p-4 h-16'>
           <div className='ml-1 text-2xl text-white mr-1 fontlogo'></div>
@@ -51,11 +53,11 @@ const Navbar = () => {
 
         {visibleMenuMobile && <div className='flex flex-col items-center bg- w-full mb-2'>
 
-          <Button onClick={() => { navigate("/") }} className={`${location.pathname === "/" ? "text-green-300" : "text-white"} text-2xl normal-case mb-2`} variant="text">Home</Button>
-          <Button onClick={() => { navigate("/tasks") }} className={`${location.pathname === "/tasks" ? "text-green-300" : "text-white"} text-2xl normal-case mb-2`} variant="text">Tasks</Button>
-          {auth.profile?.role === "SUPER_ADMIN" && <Button onClick={() => { navigate("/teams-admin") }} className={`${location.pathname === "/teams-admin" ? "text-green-300" : "text-white"} text-2xl normal-case mb-2`} variant="text">Teams - admin panel</Button>}
-          {auth.profile?.role === "SUPER_ADMIN" && <Button onClick={() => { navigate("/tasks-admin") }} className={`${location.pathname === "/tasks-admin" ? "text-green-300" : "text-white"} text-2xl normal-case mb-2`} variant="text">Tasks - admin panel</Button>}
-          {auth.profile?.role === "SUPER_ADMIN" && <Button onClick={() => { navigate("/users-admin") }} className={`${location.pathname === "/users-admin" ? "text-green-300" : "text-white"} text-2xl normal-case mb-2`} variant="text">Users - admin panel</Button>}
+          <Button onClick={() => { navigate("/") }} className={`${location.pathname === "/" ? "text-green-300" : "text-white"} text-2xl normal-case pb-2 pt-2 border-b-2 border-slate-500 border-solid w-full`} variant="text">Home</Button>
+          <Button onClick={() => { navigate("/tasks") }} className={`${location.pathname === "/tasks" ? "text-green-300" : "text-white"} text-2xl normal-case pb-2 pt-2 border-b-2 border-slate-500 border-solid w-full`} variant="text">Tasks</Button>
+          {auth.profile?.role === "SUPER_ADMIN" && <Button onClick={() => { navigate("/teams-admin") }} className={`${location.pathname === "/teams-admin" ? "text-green-300" : "text-white"} text-2xl normal-case pb-2 pt-2 border-b-2 border-slate-500 border-solid w-full`} variant="text">Teams - admin panel</Button>}
+          {auth.profile?.role === "SUPER_ADMIN" && <Button onClick={() => { navigate("/tasks-admin") }} className={`${location.pathname === "/tasks-admin" ? "text-green-300" : "text-white"} text-2xl normal-case pb-2 pt-2 border-b-2 border-slate-500 border-solid w-full`} variant="text">Tasks - admin panel</Button>}
+          {auth.profile?.role === "SUPER_ADMIN" && <Button onClick={() => { navigate("/users-admin") }} className={`${location.pathname === "/users-admin" ? "text-green-300" : "text-white"} text-2xl normal-case pb-2 pt-2 `} variant="text">Users - admin panel</Button>}
         </div>}
 
 
